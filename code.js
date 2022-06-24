6de06fecd1727d1c53cea5dd7fa4ca8bc3f9a83b
Cyril <105249525+cyrilc0de@users.noreply.github.com>
2022-06-24 18:58:45 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,15 @@
+let num1 = Number(prompt("input your first number:"));
+let num2 = Number(prompt("input your second number:"));
+let opr = prompt("what operation do you want to perform e.g +,-,*,/,");
+if(opr=="+"){
+   alert(num1 + num2)
+}
+else if (opr == "-") {
+  alert(num1 - num2)
+}
+else if (opr == "*"){
+  alert(num1*num2)
+}
+else if (opr == "/") {
+  alert(num1 / num2)
+}
